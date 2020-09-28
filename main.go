bae30e4b9ead1a8a7e5a4fb53508a0fcc79b4703
Paul Jolly <paul@myitcv.io>
2020-09-28 09:05:07 +0100

    Add checkout of code (doh)

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 
-	"cuelang.org/go/cue"
 	"github.com/kr/pretty"
 )
 
 func main() {
-	var r cue.Runtime
-	fmt.Printf("Hello world!: %v, %v", pretty.Sprint(5), r)
+	fmt.Printf("Hello world!: %v", pretty.Sprint(5))
 }
